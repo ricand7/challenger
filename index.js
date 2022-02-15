4b4ba86d085ad8eddc7e35ae2672491bd7c0aed9
ricand7 <ricantonioj@hotmail.com>
2022-02-15 17:18:11 -0300

    create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// const app = require('express')();
+const bodyParser = require('body-parser');
+
+const express = require('express');
+const app = express();
+
+const port = 3000
+
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  
+  res.status(200).json({ok: true})
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
